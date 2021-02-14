ee3043ff42745edf8cd0ac5f125bc7db07d042c3
Tyler Tracy <tyler_tracy@yaskawa.com>
2021-02-14 14:34:45 -0600

    Starter code to read water level

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 
+from modbus import ModBusSerialClient
+
+COM_PORT = "COM1"
+
 if __name__ == "__main__":
-    print("Hello, world!")
+    sensor = ModBusSerialClient(COM_PORT)
+    level_mm = sensor.read_wReg(30300)
+    print(level_mm)
